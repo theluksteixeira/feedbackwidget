62f24f8bc793483aad3bb647372d8bc24d2c90ac
Lucas Teixeira <sobrinho1@gmail.com>
2022-05-09 09:46:27 -0300

    nlw return

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,19 +1,15 @@
 import { ChatTeardropDots } from 'phosphor-react'
-import { useState } from 'react'
 import { Popover } from '@headlessui/react'
+import { WidgetForm } from './WidgetForm';
 
 export function Widget(): JSX.Element {
-    const [isWidgetOpen, setIsWidgetOpen] = useState(false);
-
-    function toogleWidgetVisibility() {
-        setIsWidgetOpen(!isWidgetOpen)
-    }
-
     return (
-        <Popover className='absolute bottom-5 right-5'>
-            {isWidgetOpen && <Popover.Panel>Hello World</Popover.Panel>}
-            
-            <Popover.Button onClick={toogleWidgetVisibility} className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
+        <Popover className='absolute bottom-4 right-4 md:bottom-8'>
+            <Popover.Panel>
+                <WidgetForm />
+            </Popover.Panel>
+
+            <Popover.Button className='bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group'>
                 <ChatTeardropDots className='w-6 h-6'></ChatTeardropDots>
 
                 <span className='max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear'>
---
+++ src/components/WidgetForm.tsx
@@ -0,0 +1,49 @@
+import { CloseButton } from "./CloseButton";
+
+import bugImageUrl from '../assets/bug.svg';
+import ideaImageUrl from '../assets/idea.svg';
+import thoughtImageUrl from '../assets/thought.svg';
+
+const feedBackTypes = {
+    BUG: {
+        title: 'Problema',
+        image: {
+            source: bugImageUrl,
+            alt: 'Imagem de um inseto'
+        }
+    },
+    IDEA: {
+        title: 'Ideia',
+        image: {
+            source: ideaImageUrl,
+            alt: 'Imagem de uma lâmpada'
+        }
+    },
+    OTHER: {
+        title: 'Outro',
+        image: {
+            source: thoughtImageUrl,
+            alt: 'Imagem de um Balão de pensamento'
+        }
+    }
+}
+
+export function WidgetForm() {
+    return (
+        <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
+            <header>
+                <span className="text-xl leading-6">Deixei seu feedback</span>
+
+                <CloseButton />
+            </header>
+
+            <div className="flex py-8 gap-2 w-full">
+
+            </div>
+
+            <footer className="text-xs text-neutral-400">
+                Feito com ❤️ pela <a className="underline underline-offset-2" href="">Rocketseat</a>
+            </footer>
+        </div>
+    )
+}
