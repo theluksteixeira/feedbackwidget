d7dbff3f87ece013b53061d2a551f4201ad8431a
Lucas Teixeira <sobrinho1@gmail.com>
2022-05-24 09:44:41 -0300

    nlw-return

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routers';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(routes);
 
 app.listen(3333, () => {
