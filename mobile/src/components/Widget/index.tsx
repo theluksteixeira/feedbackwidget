33e6b865e7d15ac66bb3a6da3b488a06572463a6
Lucas Teixeira <sobrinho1@gmail.com>
2022-05-26 09:57:14 -0300

    mobile

---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import { styles } from './styles';
+
+export function Widget() {
+  return (
+    <View style={styles.container}>
+      <Text>Lucas</Text>
+    </View>
+  );
+}
